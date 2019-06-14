0fbad129a6e7ec482892cf6da79e4e9792603fd3
Kuldeep Verma <kulldeep.verma@users.noreply.github.com>
2019-06-14 12:41:04 +0400

    SaveSharePointListAsTemplate(with and without data)

--- SaveListAsTML.ps1
+++ SaveListAsTML.ps1
@@ -5,29 +5,27 @@ Title           : Save list as template
 Description     : This is to save SharePoint list as template. 
 #>
 
-
 #to ensure the SharePoint PowerShell snapin is loaded
 if($null -eq (Get-PSSnapin "Microsoft.SharePoint.PowerShell"))
 {
     Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue 
 }
-
 #variable declaration
-$TMLName="MedicalInsuranceTML"
-$TMLFileName="MedicalInsuranceTML"
-$TMLDescription="Saving before doing any changes in List."
-$WebURL = "http://lgvsa001:2013/app/Mi"
-$ListName="MedicalInsurance"
+$TMLName="<template name>"
+$TMLFileName="<template file name>"
+$TMLDescription="<description of the template>"
+$WebURL = "<web url>" 
+$ListName="<list name>"
 
 #Pass $true for template with data and $false for template without data.
 $SaveData = $true
-
-##Get the Web and List objects
+#Get web object of SharePoint site
 $Web = Get-SPWeb $WebURL
+#Get list object from web
 $List = $Web.Lists[$ListName]
 
 ##Save List as Template
 $List.SaveAsTemplate($TMLFileName, $TMLName, $TMLDescription, $SaveData)
-Write-Host "List Saved as Template!" 
+Write-Host "List Saved as Template!" -ForegroundColor Green
 
 
