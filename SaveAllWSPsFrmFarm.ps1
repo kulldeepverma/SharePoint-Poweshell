b93e613b1d2af34f00428a90adc7f10a39e4e6ac
Kuldeep Verma <39624249+kulldeepverma@users.noreply.github.com>
2019-06-15 21:41:30 +0400

    no need.
    no need.

--- SaveAllWSPsFrmFarm.ps1
+++
@@ -1,5 +0,0 @@
-$folder="E:\Production_Deployment\"
-$WspName="DEWA_SyncXLtoDB"
-$farm = Get-SpFarm 
-$file = $farm.Solutions.Item($WspName+".wsp").SolutionFile 
-$file.SaveAs($folder + $WspName +"-"+ [DateTime]::Now.ToString("yyyyMMdd-HHmmss") + ".wsp")
